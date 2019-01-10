817a9ef5e70e4f4fc22b4c68c0ee3320f14ef5ed
Sarthak Mangla <37767310+knightron0@users.noreply.github.com>
2019-01-10 09:58:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import net
-from flask import Flask
+from flask import Flask, redirect, request, json, render_template
 
+#flask basic code 
+app = Flask(__name__)
+
+@app.route('/', methods = ["GET", "POST"])
+def dashboard():
+    print(net.called)
+    return "hsllo"
+if __name__ == "__main__":
+    app.run()
--- net.py
+++ net.py
@@ -1,8 +1,10 @@
+def ran():
+    return random.randint(0, 58)
+
 import tensorflow as tf
-import pandas as pd
 import numpy as np
 import datann
-
+import random
 
 data = datann.data
 output = datann.output
@@ -10,40 +12,56 @@ n_nodes_hl1 = 1
 n_classes = 1
 batch_size = 100
 
-lol = 1
-x = tf.placeholder('float', [1, 5])
-y = tf.placeholder('float')
 
+
+x = tf.placeholder('float', [1, 5], name="x")
+y = tf.placeholder('float', name="y")
 def net(data):
-    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([5,n_nodes_hl1,])),
-                    'biases':tf.Variable(tf.random_normal([n_nodes_hl1]))}
-    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_classes ])),
-                    'biases':tf.Variable(tf.random_normal([n_classes])),}
+    hidden_1_layer = {'weights':tf.Variable(tf.random_normal([5,n_nodes_hl1]), name="hl1w"),
+                    'biases':tf.Variable(tf.random_normal([n_nodes_hl1]), name="hl1b")}
+    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_classes ]), name="ol1w"),
+                    'biases':tf.Variable(tf.random_normal([n_classes]), name="ol1b")}
     l1 = tf.add(tf.matmul(data,hidden_1_layer['weights']), hidden_1_layer['biases'])
     l1 = tf.nn.relu(l1)
-    output = tf.matmul(l1, output_layer['weights']) + output_layer['biases']
-
+    output = tf.add(tf.matmul(l1, output_layer['weights']), output_layer['biases'], name="output")
+    saver = tf.train.Saver()
     return output
 
 
 def train(x):
     global data
     prediction = net(x)
-    cost = tf.reduce_mean(-y * tf.log(prediction) - (1-y) * tf.log(1-prediction))
+    cost = tf.reduce_mean((y-prediction)**2)
     optimizer = tf.train.AdamOptimizer().minimize(cost)
-    print(cost)
-    hm_epochs = 5
+    hm_epochs = 10
+    saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for epoch in range(hm_epochs):
+        for epoch in range(100):
             epoch_loss = 0
-            for _ in range(int(500/batch_size)):
-                epoch_x, epoch_y = [data[_]],output[_]
-                _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
+            pred = 0
+            for i in range(int(1200)):
+                epoch_x = [data[i]]
+                epoch_y = output[i]
+                _, c, p = sess.run([optimizer, cost, prediction], feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
+                print(c, p, epoch_y)
 
-            print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
+            print('Epoch', epoch, 'completed out of 100, ','loss:',epoch_loss, pred)
+
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y,1 ))
-        #accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float')) 
+        save_path = saver.save(sess, "model/modeltf.ckpt")
 
-train(x)
+#train(x)
+def called():
+    with tf.Session() as sess:
+        graph = tf.get_default_graph()
+        saver = tf.train.import_meta_graph('model/modeltf.ckpt.meta') 
+        saver.restore(sess, "model/modeltf.ckpt")
+        x_1 = graph.get_tensor_by_name("x_1:0")
+        xplace  =[[0.0, 0.0,0.0, 0.0, 0.0]]
+        prediction1 = graph.get_tensor_by_name("output:0")
+        p = sess.run([prediction1], feed_dict = {x_1: xplace})
+        ran()
+called()
